6fbd6a367a6ea4c1cba4916c967ed44ada54e4da
Venkata Saketh Teki <venkatsakethteki@gmail.com>
2022-03-27 22:56:04 +0000

    4:26

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,11 +1,30 @@
-with payments as (
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
 
+payments as (
     select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
 
-select 
-    payments.order_id,
-    payments.customer_id,
-    payments.amount
-
-from payments
+select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,8 +1,11 @@
 select
-    id as customer_id,
+    id as payment_id,
     orderid as order_id,
-    paymentmethod,
+    paymentmethod as payment_method,
     status,
-    amount,
-    created
-from raw.stripe.payment
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment 
