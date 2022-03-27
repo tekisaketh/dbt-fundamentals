3543e96466e7f607b1c6a4927fa9b050885b3f6d
Venkata Saketh Teki <venkatsakethteki@gmail.com>
2022-03-27 19:02:37 +0000

    updated with -test string in myfirstdbtmodel

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -2,7 +2,7 @@
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
+    This will override configurations stated in dbt_project.yml - test
 
     Try changing "table" to "view" below
 */
